d455bb5952e5a12ff7da0e319a3d397859dcb583
quaziadib <quaziadib@gmail.com>
2022-01-16 17:46:39 +0600

    slight edit

--- De-Rain-Main-Part/training.py
+++ De-Rain-Main-Part/training.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 
 torch.cuda.empty_cache()
 
+
 def main():
     disc = Discriminator().to(config.DEVICE)
     gen = Generator().to(config.DEVICE)
@@ -35,35 +36,33 @@ def main():
         for _, (x, y) in enumerate(loop):
             x, y = x.to(config.DEVICE), y.to(config.DEVICE)
 
-            
             with torch.cuda.amp.autocast():
                 y_fake = gen(x)
                 D_real = disc(x, y)
                 D_fake = disc(x, y_fake.detach())
-                D_real_loss = BCE(D_real, torch.ones_like(D_real)) 
-                D_fake_loss = BCE(D_fake, torch.zeros_like(D_fake)) 
-                D_loss = (D_real_loss + D_fake_loss) / 2 
-        
+                D_real_loss = BCE(D_real, torch.ones_like(D_real))
+                D_fake_loss = BCE(D_fake, torch.zeros_like(D_fake))
+                D_loss = (D_real_loss + D_fake_loss) / 2
+
             disc.zero_grad()
             d_scaler.scale(D_loss).backward()
             d_scaler.step(opt_disc)
             d_scaler.update()
-        
+
             with torch.cuda.amp.autocast():
                 D_fake = disc(x, y_fake)
                 G_fake_loss = BCE(D_fake, torch.ones_like(D_fake))
                 L1 = L1_LOSS(y_fake, y) * config.L1_LAMBDA
-                G_loss = G_fake_loss + L1 
-        
+                G_loss = G_fake_loss + L1
+
             opt_gen.zero_grad()
             g_scaler.scale(G_loss).backward()
             g_scaler.step(opt_gen)
             g_scaler.update()
-           
 
         if epoch % 5 == 0:
-            torch.save(gen, f'model_weights/gen_{_}.pth')
-            torch.save(disc, f'model_weights/disc_{_}.pth')
+            torch.save(gen, f'model_weights/gen_{epoch}.pth')
+            torch.save(disc, f'model_weights/disc_{epoch}.pth')
 
 
 if __name__ == '__main__':
